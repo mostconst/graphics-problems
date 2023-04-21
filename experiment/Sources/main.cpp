5644c1767bfe3690e5b10ba90607511316374e18
mostconst <113008093+mostconst@users.noreply.github.com>
2023-04-21 15:58:58 +0300

    заготовка

---
+++ experiment/Sources/main.cpp
@@ -0,0 +1,229 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+#include "ShaderProgram.h"
+#include "gldebug.h"
+#include "ArrayBuffer.h"
+#include "VertexArray.h"
+#include "DrawData.h"
+#include "GeometryObject.h"
+#include "UserContext.h"
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+void mouse_callback(GLFWwindow* window, double screenX, double screenY);
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+
+nsk_cg::VertexArray LoadBuffers(const nsk_cg::GeometryObject& object, std::vector<nsk_cg::ArrayBuffer>& arrayBuffers,
+                        std::vector<nsk_cg::ElementBuffer>& elementBuffers)
+{
+    const auto& colors = object.GetColors();
+    const auto& vertices = object.GetVertices();
+    const auto& indices = object.GetIndices();
+    using ColorType = std::remove_reference_t<decltype(colors)>::value_type;
+    using VertexType = std::remove_reference_t<decltype(vertices)>::value_type;
+    using IndexType= std::remove_reference_t<decltype(indices)>::value_type;
+
+    nsk_cg::ArrayBuffer positionsVBO;
+    positionsVBO.bufferData(vertices.size() * sizeof(VertexType), vertices.data(),
+                            GL_STATIC_DRAW);
+    nsk_cg::ArrayBuffer colorsVBO;
+    colorsVBO.bufferData(colors.size() * sizeof(ColorType), colors.data(),
+                         GL_STATIC_DRAW);
+
+    // HW_ITEM 7
+    constexpr int positionIndex = 0;
+    nsk_cg::VertexArray vao;
+    vao.vertexAttribPointer(positionsVBO, positionIndex, VertexType::nComponents, VertexType::componentType, GL_FALSE, 0, 0);
+    vao.enableVertexAttribArray(positionIndex);
+
+    constexpr int colorIndex = 1;
+    vao.vertexAttribPointer(colorsVBO, colorIndex, ColorType::nComponents, ColorType::componentType, GL_FALSE, 0, 0);
+    vao.enableVertexAttribArray(colorIndex);
+
+    nsk_cg::ElementBuffer ebo;
+    ebo.bufferData(vao, indices.size() * sizeof(IndexType),
+                   object.GetIndices().data(),
+                   GL_STATIC_DRAW);
+
+    elementBuffers.push_back(std::move(ebo));
+    arrayBuffers.push_back(std::move(positionsVBO));
+    arrayBuffers.push_back(std::move(colorsVBO));
+
+    return vao;
+}
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifndef NDEBUG
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);
+#endif
+
+    constexpr glm::vec3 firstCubeLocation(1.0f, 1.0f, 0.5f);
+    constexpr glm::vec3 secondCubeLocation(-1.0f, -1.0f, 0.5f);
+    constexpr float platformSize = 5.0f;
+    constexpr glm::vec3 lookAt(-1.0f, -1.0f, 0.0f);
+    // HW_ITEM 3
+    nsk_cg::UserContext userContext(lookAt, 5.5f);
+
+    GLFWwindow* window = glfwCreateWindow(userContext.GetScreenWidth(), userContext.GetScreenHeight(), "Problem 1",
+                                          nullptr, nullptr);
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetMouseButtonCallback(window, mouse_button_callback);
+    glfwSetWindowUserPointer(window, &userContext);
+
+    if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+#ifndef NDEBUG
+    nsk_cg::enableDebugOutput();
+#endif
+
+    nsk_cg::ShaderProgram ourShader;
+    try
+    {
+        ourShader = nsk_cg::createShader("shader.vs", "shader.fs");
+    }
+    catch (std::ifstream::failure& e)
+    {
+        std::cout << "Failed to read shader file: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    catch (std::invalid_argument& e)
+    {
+        std::cout << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    float vertices[] = {
+         0.5f,  0.5f, 0.0f,  // top right
+         0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left 
+    };
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,  // first Triangle
+        1, 2, 3   // second Triangle
+    };
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0);
+
+
+    // uncomment this call to draw in wireframe polygons.
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+        // -----
+        processInput(window);
+
+        // render
+        // ------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // draw our first triangle
+        ourShader.Use();
+        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        //glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        // glBindVertexArray(0); // no need to unbind it every time 
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+
+    glfwTerminate();
+    return EXIT_SUCCESS;
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+void framebuffer_size_callback(GLFWwindow* window, const int width, const int height)
+{
+    auto* context = static_cast<nsk_cg::UserContext*>(glfwGetWindowUserPointer(window));
+    context->OnWindowSizeChange(width, height);
+}
+
+void mouse_callback(GLFWwindow* window, const double screenX, const double screenY)
+{
+    auto* context = static_cast<nsk_cg::UserContext*>(glfwGetWindowUserPointer(window));
+    context->OnMouseMove(screenX, screenY);
+}
+
+void mouse_button_callback(GLFWwindow* window, const int button, const int action, int)
+{
+    auto* context = static_cast<nsk_cg::UserContext*>(glfwGetWindowUserPointer(window));
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+    {
+        context->OnLeftMouseButtonAction(true);
+    }
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE)
+    {
+        context->OnLeftMouseButtonAction(false);
+    }
+}
